258067243ba2601c38ba5437f1b2db264fddd2e3
Nikhil Mahajan <codernikhil77@gmail.com>
2022-08-23 14:35:03 +0530

    Updated New Files

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.io.*;
+  class Addition {
+    public static void main(String[] args)
+    {
+        int num1 = 100, num2 = 20, sum = 0;        // initializing variables
+
+  
+         System.out.println("num1 = " + num1);    // Displaying num1 and num2
+
+        System.out.println("num2 = " + num2);    // Displaying num1 and num2
+
+  
+        sum = num1 + num2;	                             // adding num1 and num2
+
+        System.out.println("The sum = " + sum);
+    }
+}
